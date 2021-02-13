b0d42d7c9e12597f19211eb3e3d78f7f02a2b1bb
cekun <cekung@gmail.com>
2021-02-13 11:38:17 +0800

    combineReducers补充

--- lesson2/src/store/index.js
+++ lesson2/src/store/index.js
@@ -1,5 +1,5 @@
-import {createStore, applyMiddleware, combineReducers} from "redux";
-// import {createStore, applyMiddleware} from "../kredux/";
+// import {createStore, applyMiddleware, combineReducers} from "redux";
+import {createStore, applyMiddleware, combineReducers} from "../kredux/";
 // import thunk from "redux-thunk"; // 异步解决方案
 // import logger from "redux-logger"; // 打印日志
 // import promise from "redux-promise"; // 处理promise
@@ -17,10 +17,24 @@ function countReducer(state = 0, action) {
   }
 }
 
+function countReducer2(state = 0, action) {
+  switch (action.type) {
+    case "ADD":
+      return state + 1;
+    case "MINUS":
+      return state - action.payload || 1;
+    default:
+      return state;
+  }
+}
+
 // logger要作为applyMiddleware的最后一个参数，不然不能保证action是plain object
 const store = createStore(
-  // countReducer,
-  combineReducers({count: countReducer}),
+  //countReducer,
+  combineReducers({
+    count: countReducer,
+    count2: countReducer2
+  }),
   applyMiddleware(promise, thunk, logger)
 );
 
@@ -45,7 +59,7 @@ function logger({dispatch, getState}) {
     const returnValue = next(action);
 
     const nextState = getState();
-    console.log("prev state", nextState); //sy-log
+    console.log("next state", nextState); //sy-log
 
     console.log("++++++++++++++++++++++++++"); //sy-log
 
@@ -68,3 +82,11 @@ function promise({dispatch}) {
     return isPromise(action) ? action.then(dispatch) : next(action);
   };
 }
+
+// function pro({dispatch}) {
+//   return function(next) {
+//     return function(action) {
+//       return isPromise(action) ? action.then(dispatch) : next(action);
+//     }
+//   }
+// }
